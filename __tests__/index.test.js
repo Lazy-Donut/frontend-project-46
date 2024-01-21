73915210d453dd4e1744b68c17c7ffb448b0e50e
Lazy-Donut <lazy.donut@yandex.ru>
2024-01-21 21:36:56 +0600

    Refactored tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -23,11 +23,13 @@ test('should return the correct difference string', () => {
 test('should return the correct difference string with absolute path', () => {
   const file1 = path.resolve(__dirname, '../__fixtures__/file1.json');
   const file2 = path.resolve(__dirname, '../__fixtures__/file2.json');
-  const expectedDiff = `- follow: false
-  host: hexlet.io
-- proxy: 123.234.53.22
-- timeout: 50
-+ timeout: 20
-+ verbose: true`;
+  const expectedDiff = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
   expect(genDiff(file1, file2)).toEqual(expectedDiff);
 });
